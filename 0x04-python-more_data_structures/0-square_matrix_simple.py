cb0db96a922121cfc2dfc9d5c1932959c1863b4e
Noah Alemayehu <noahlem21@gmail.com>
2023-10-11 10:04:22 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     res = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-    length  = len(matrix)
+    length = len(matrix)
     for i in range(length):
         for j in range(len(matrix[0])):
             res[i][j] = matrix[i][j] ** 2

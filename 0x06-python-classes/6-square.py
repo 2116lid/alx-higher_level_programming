1c01e5e2c0d5dd8b6f55adf1c5b8c4d4330c27af
Noah Alemayehu <noahlem21@gmail.com>
2023-10-25 14:07:18 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,8 +50,8 @@ class Square:
         """prints # character on stdout """
         if self.__size == 0:
             print("")
-        else:
-            for i in range(self.__position[1]):
-                print("")
-            for j in range(self.__size):
-                print("" * self.position[0] + "#" * self.__size)
+        [print("") for i in range(self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for l in range(self.__size)]
+            print("")

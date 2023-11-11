f90faa97c4a4e1be6aff08857a8b83f1409e02bc
Noah Alemayehu <noahlem21@gmail.com>
2023-11-11 08:13:18 -0800

    testupdate

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,7 +9,10 @@ Unittest classes:
     TestBase_load_from_file
 """
 import os
+import sys
 import unittest
+from unittest.mock import patch
+from io import StringIO
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
@@ -304,6 +307,123 @@ class TestBase_load_from_file(unittest.TestCase):
     def test_load_from_file_more_than_one_arg(self):
         with self.assertRaises(TypeError):
             Base.load_from_file([], 1)
+
+class TestRectangleInstantiationAndAttributes(unittest.TestCase):
+    """Test suite for Rectangle instantiation and attributes in
+    models.rectangle"""
+
+    def test_rectangle_is_subclass_of_base(self):
+        self.assertTrue(issubclass(Rectangle, Base))
+
+    def test_error_raised_when_no_arg_is_passed(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle()
+
+    def test_error_raised_when_one_arg_is_passed(self):
+        with self.assertRaises(TypeError):
+            rectangle = Rectangle(1)
+
+    def test_width_can_be_set_though_private(self):
+        rectangle = Rectangle(1, 2)
+        rectangle.width = 2
+        self.assertEqual(2, rectangle.width)
+
+    def test_height_can_be_set_though_private(self):
+        rectangle = Rectangle(1, 2)
+        rectangle.height = 2
+        self.assertTrue(2, rectangle.height)
+
+    def test_id_increment_by_one_if_not_given(self):
+        rect1 = Rectangle(1, 2)
+        rect2 = Rectangle(3, 2)
+        self.assertEqual(rect2.id - rect1.id, 1)
+
+    def test_error_is_raised_if_width_not_int(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(3.2, 2)
+
+    def test_error_is_raised_if_height_not_int(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, "s")
+
+    def test_error_is_raised_if_x_not_int(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, 2, True)
+
+    def test_error_is_raised_if_y_not_int(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(1, 2, y="g")
+
+    def test_width_not_under_nor_equals_0(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-1, 3)
+
+    def test_height_not_under_nor_equals_0(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(2, 0)
+
+    def test_x_not_under_0(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(2, 1, -1)
+
+    def test_y_not_under_0(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(2, 1, y=-1)
+
+    def test_miscelleneous(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-1, 2)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(1, -2)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(2, 0)
+
+class TestRectangleMethods(unittest.TestCase):
+    """A test suite for the Rectangle methods"""
+
+    def setUp(self):
+        """Set up instances and data for this class"""
+        self.r1 = Rectangle(3, 2)
+        self.r2 = Rectangle(2, 10)
+        self.r3 = Rectangle(8, 7, 0, 0, 12)
+
+    def test_area(self):
+        self.assertEqual(self.r1.area(), 6)
+        self.assertEqual(self.r2.area(), 20)
+        self.assertEqual(self.r3.area(), 56)
+
+    def test_display(self):
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            self.r1.display()
+            self.assertEqual(fake_stdout.getvalue(), '###\n###\n')
+
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            self.r1.update(2, 3, 2, 2, 2)
+            self.r1.display()
+            self.assertEqual(fake_stdout.getvalue(), "\n\n  ###\n  ###\n")
+
+    def test_str(self):
+        string = str(self.r1)
+
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            print(self.r1, end="")
+            self.assertEqual(fake_stdout.getvalue(), string)
+
+    def test_update(self):
+        self.r1.update(23, 2, 3, 4)
+
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            print(self.r1, end="")
+            self.assertEqual(fake_stdout.getvalue(), str(self.r1))
+
+        self.r1.update()
+        self.assertEqual(self.r1.to_dictionary(),
+        {"id": 23, "width": 2, "height": 3, "x": 4, "y": 0})
+        self.r1.update(89)
+        self.assertEqual(self.r1.to_dictionary(),
+        {"id": 89, "width": 2, "height": 3, "x": 4, "y": 0})
 
 if __name__ == "__main__":
     unittest.main()

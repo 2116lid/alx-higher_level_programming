074ab1fc50314ff9953fe776cfe600e408ad8ba3
Noah Alemayehu <noahlem21@gmail.com>
2024-01-08 09:33:04 -0800

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 if (typeof argv[2] === 'string' && !isNaN(argv[2])) {
   console.log('My number: ' + parseInt(argv[2]));
 } else if (typeof argv[2] !== 'number' || argv[2] === undefined) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else if (typeof argv[2] === 'number') {
   console.log('My number: ' + Math.floor(argv[2]));
 } else {

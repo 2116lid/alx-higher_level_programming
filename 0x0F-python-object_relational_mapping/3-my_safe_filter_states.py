74e21db0a403af9b1f433b3945ce9a6c910ccee5
Noah Alemayehu <noahlem21@gmail.com>
2024-01-14 03:08:02 -0800

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    c = conn.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                   (sys.argv[4],))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    conn.close()

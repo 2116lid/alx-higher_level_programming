32ef5bd0c420353410fe4fd6f94833184a755ac6
Noah Alemayehu <noahlem21@gmail.com>
2024-01-14 03:45:10 -0800

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that lists all cities from the database hbtn_0e_4_usa """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    c = conn.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)

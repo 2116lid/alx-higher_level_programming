30f57e7b700a776d50e4c07812b4d7f7e46c79a1
Noah Alemayehu <noahlem21@gmail.com>
2024-01-25 14:40:51 -0800

    taskFix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and
-# displays the size of the body of the response.
-
+# Gets the size of the body of a response from a URL
 curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2

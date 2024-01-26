2f562860b0c886968d86373b2053a0fe030f83c8
Noah Alemayehu <noahlem21@gmail.com>
2024-01-26 01:26:02 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as rep:
     fet = rep.read()
-print("Body response:\n  - type: {}".format(type(fet)))
-print('  - content: {}'.format(fet))
-print('  - utf8 content: {}'.format(fet.decode('utf-8')))
+print("Body response:\n\t- type: {}".format(type(fet)))
+print('\t- content: {}'.format(fet))
+print('\t- utf8 content: {}'.format(fet.decode('utf-8')))
